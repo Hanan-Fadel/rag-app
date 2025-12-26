012a79a164d8eb8d8c2142b6dff88c0ef61f3265
Hanan-Fadel <fadel_hanan@yahoo.com>
2025-12-26 18:46:59 -0500

    Setup database and define schema

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
-
 from routes import base, data
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.config import get_settings, Settings
+
+app_settings = get_settings()
+
+mongo_client = AsyncIOMotorClient(app_settings.MONGODB_URL)
+mongo_db = mongo_client[app_settings.MONGODB_DATABASE]
+
+
 app = FastAPI() 
+
+@app.on_event("startup")
+async def startup_db_client():
+    settings = get_settings()
+    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+   app.mongo_conn.close()
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
---
+++ src/models/db_schemas/Project.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field, validator
+from typing import Optional
+from bson.objectid import ObjectId
+
+
+class Project(BaseModel):
+    _id: Optional[ObjectId] 
+    project_id: str = Field(..., min_length=1)
+
+    @validator('project_id') #custom validator on the schema
+    def validate_project_id(cls, value):
+        if not value.isalnum(): #project id should be alphanumeric
+            raise ValueError("Project ID should be alphanumeric")
+        return value
+
+    class Config:
+        arbitrary_types_allowed = True  #allow to use ObjectId in the schema as a field type
