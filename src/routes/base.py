833a40663883528df0269823cf2e64b116121720
Hanan-Fadel <fadel_hanan@yahoo.com>
2025-12-25 20:55:50 -0500

    create upload file endpoint

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, APIRouter
-import os
+from fastapi import FastAPI, APIRouter, Depends
+
+from helpers.config import get_settings, Settings
 
 
 base_router = APIRouter(
@@ -8,10 +9,8 @@ base_router = APIRouter(
 )
 
 @base_router.get("/") #default route
-async def welcome(): 
-    app_name= os.getenv("APP_NAME")
-    app_version= os.getenv("APP_VERSION")
+async def welcome(app_settings: Settings =Depends(get_settings)): #app_settings: Settings means app_settings of type Settings
     return {
-        "app_name": app_name,
-        "app_version": app_version
+        "app_name": app_settings.APP_NAME,
+        "app_version": app_settings.APP_VERSION
     }
